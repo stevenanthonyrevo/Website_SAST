ee2fb056ec964e7a505c1176b497524f93e30b08
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-08-23 13:32:13 +0530

    Footer Seprated form landing page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,65 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import "../index.css";
+import React from 'react'
 
-const Login = () => {
-  const [isLogin, setIsLogin] = useState(true);
-
+function Login() {
   return (
-    <div className="login-body">
-      <div className="login-wrapper">
-        <div className="login-box animate-login">
-          <div className="auth-tabs">
-            <button
-              className={`auth-tab ${isLogin ? "active" : ""}`}
-              onClick={() => setIsLogin(true)}
-            >
-              Log In
-            </button>
-            <button
-              className={`auth-tab ${!isLogin ? "active" : ""}`}
-              onClick={() => setIsLogin(false)}
-            >
-              Sign Up
-            </button>
-          </div>
-
-          <h1 className="login-title">
-            {isLogin ? "Welcome Back" : "Join Us"}
-          </h1>
-          <p className="login-subtitle">
-            {isLogin ? "Log in to your account" : "Create a new account"}
-          </p>
-
-          <form className="login-form">
-            <input type="email" placeholder="Email" required />
-            {!isLogin && <input type="text" placeholder="Username" required />}
-            <input type="password" placeholder="Password" required />
-
-            {!isLogin && (
-              <input type="password" placeholder="Confirm Password" required />
-            )}
-
-            <button type="submit" className="auth-button">
-              {isLogin ? "Log In" : "Sign Up"}
-            </button>
-          </form>
-
-          {isLogin ? (
-            <p className="signup-text">
-              Don’t have an account?{" "}
-              <span onClick={() => setIsLogin(false)}>Sign up</span>
-            </p>
-          ) : (
-            <p className="signup-text">
-              Already have an account?{" "}
-              <span onClick={() => setIsLogin(true)}>Log in</span>
-            </p>
-          )}
-        </div>
-      </div>
+    <div>
+      hii
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
