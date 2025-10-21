d084a9a4aaa6005d5aaf1135c694e886a3aa9f94
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-21 11:08:21 +0530

    Removed dupliacte register in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,13 +70,9 @@ const Navbar = () => {
                   Members
                 </Link>
               </li>
-              <li>
-                <Link to="/Register">Register</Link>
-              </li>
-
               <li><a href="/news">Astronomy News</a></li>
               <li><a href="/track">Track</a></li>
-              <Link to="/Register">Register</Link>
+              <li><Link to="/Register">Register</Link></li>
             </ul>
           </nav>
 
